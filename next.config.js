a40abc3accfca8c8ee7143fb1821e63868b02c1b
Vishalr2312 <vishal@renderinfotech.com>
2026-02-19 13:39:57 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
+  // output: "export",
   images: {
     domains: ["techsocial.s3.ap-south-1.amazonaws.com"],
     unoptimized: true,
